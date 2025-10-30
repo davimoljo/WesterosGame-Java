f90329f3f5908b4e3269400a3b4fbe3093defd9e
João Pedro <joao.guadalupe06@gmail.com>
2025-10-30 12:07:29 -0300

    correção print de dano no stark

--- src/main/java/westeros/personagens/Stark.java
+++ src/main/java/westeros/personagens/Stark.java
@@ -21,10 +21,11 @@ public class Stark extends Personagem{
     @Override
     public void receberDano(int dano){
         // Aplica o dano (BUG: 'dano - defesa' pode curar; BUG 2: imprime 'dano' bruto)
-        vida -= (int)((dano - defesa) * 0.8);
+        int danoReal = (int)((dano - defesa) * 0.8);
+        vida -= danoReal;
 
         // Imprime o feedback de dano (NOTA: está imprimindo o dano bruto, não o dano real sofrido)
-        System.out.println("\n>>> " + this.getNome() + " sofreu " + dano + " de dano! (Habilidade Stark ativada!)");
+        System.out.println("\n>>> " + this.getNome() + " sofreu " + danoReal + " de dano! (Habilidade Stark ativada!)");
         this.imprimeStatus(); // Mostra o status atualizado.
     }
 }
